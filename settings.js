e4327469b14294069cde307c438351fd725528a8
Bill Doody <wdoody@yahoo.com>
2018-08-02 09:13:48 -0600

    auth0 up and running

---
+++ settings.js
@@ -0,0 +1,7 @@
+const clientID = process.env.CLIENTID || 'tvVNrCD9w2qChzBM8NrAdktDgpdDfk8b'; // your clientID
+const domain = process.env.DOMAIN || 'doodcoin.auth0.com'; // your domain
+
+export {
+    clientID,
+    domain
+};
